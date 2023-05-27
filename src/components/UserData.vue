17e3de37f145a98fbeeef7c10244afbd419efae2
César MORO <cesar.moro.dev@gmail.com>
2023-05-27 15:43:14 +0200

    mise en place du Vew-model et du Model pour le SFC UserData. Les données utilisateurs fonctionnnet et renvoient correctement le métabolisme de base et la Dépense Énergétique Journalière. On peut donc passer à la prochaine grosse étape

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -2,82 +2,159 @@
 <section class="add-user-data">
         <p>Merci de renseigner les informations suivantes :</p>  
 
-        <form method="POST" class="data-form">
+        <form class="data-form" method="POST">
                 <div class="data-block">
                 
                         <div class="data-div data-gender">
                                 <label class="data-label">Votre genre :</label>
-                                <input class="data-input" type="radio" name="sex" value="man" id="boy" required>
+                                <input v-model="gender" class="data-input" type="radio" name="sex" value="man" id="boy" required> <!-- C'est l'attr "value" des <input type="radio"> qui est passé au Model dans la propriété "gender" -->
                                 <label class="data-label-proposition" for="boy">Homme</label>
-                                <input class="data-input" type="radio" name="sex" value="woman" id="girl" required>
+                                <input v-model="gender" class="data-input" type="radio" name="sex" value="woman" id="girl" required>
                                 <label class="data-label-proposition" for="girl">Femme</label>
                         </div>
 
                         <div class="data-div data-weight">
                                 <label class="data-label">Votre poids :</label>
-                                <input class="data-input" type="number" required> Kg (ex. : "87.60")
+                                <input v-model="weight" class="data-input" type="number" step="0.01" required> Kg (ex. : "87.60")
                         </div>
 
                         <div class="data-div data-height">
                                 <label class="data-label">Votre taille :</label>
-                                <input class="data-input" type="number" required> m (ex. : "1.80")
+                                <input v-model="height" class="data-input" type="number" step="0.01" required> m (ex. : "1.80")
                         </div>
                         
                         <div class="data-div data-age">
                                 <label class="data-label">Votre âge :</label>
-                                <input class="data-input" type="number" required> ans (ex. : "34"")
+                                <input v-model="age" class="data-input" type="number" required> ans (ex. : "34"")
                         </div>
                         
                         <div class="data-div data-nap-level">
                                 <label class="data-label" for="nap">Votre niveau d'activité physique :</label>
-                                <select name="nap-name" id="nap-id" required>
-                                        <option value="">Veuillez choisir une option</option>
-                                        <option value="0">activité physique nulle</option>
-                                        <option value="1">activité physique minimale</option>
-                                        <option value="2">activité physique modérée</option>
-                                        <option value="3">activité physique forte</option>
-                                        <option value="4">activité physique intense</option>
+                                <select v-model="napLevel" name="nap-name" id="nap-id" required> <!-- C'est l'attr "value" des <option></option> qui est passé au Model dans la propriété "napLevel"-->
+                                        <option>Veuillez choisir une option</option>
+                                        <option value="inactif">Inactif</option>
+                                        <option value="moderate">activité physique modérée</option>
+                                        <option value="important">activité physique importante</option>
+                                        <option value="intense">activité physique intense</option>
                                 </select>
 
-                                <p class="data-label display-none" type="number" required>À l'aide du tableau "Niveau d'Activité Physique", précisez le coefficient selon que vous soyez plus proche du niveau d'activité inférieur ou supérieur (ex. vous avez renseigné "activité physique forte". Si vous êtes plus proche du niveau inférieur, sélectionnez le coefficient 1.6. Si vous êtes plus proche du niveau supérieur, sélectionnez le coefficient 1.8). 
-                                </p>
-                                <div class="data-div  data-nap-coef display-none">
-                                        <label class="data-label">Votre coefficient :</label>
+                                <div v-if="napLevel" class="data-div  data-nap-coef">
+                                        <p class="data-label display-none" type="number" required>À l'aide du tableau "Niveau d'Activité Physique", précisez le coefficient selon que vous soyez plus proche du niveau d'activité inférieur ou supérieur (ex. vous avez renseigné "activité physique forte". Si vous êtes plus proche du niveau inférieur, sélectionnez le coefficient 1.6. Si vous êtes plus proche du niveau supérieur, sélectionnez le coefficient 1.8). 
+                                        </p>
+                                        <label class="data-label">Précisez votre coefficient :</label>
 
-                                        <div class="data-nap-coef__level">
-                                                <input class="data-input" type="radio" name="nap" value="null-minus" id="null-minus" required>
-                                                <label class="data-label-proposition" for="null-minus">1.0</label>
-                                                <input class="data-input" type="radio" name="nap" value="null-plus" id="null-plus" required>
-                                                <label class="data-label-proposition" for="null-plus">1.1</label>
+                                        <div class="data-nap-coef__level" v-if="napLevel == 'inactif'">
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="1.3" id="inactif-low" required>
+                                                <label class="data-label-proposition" for="inactif-low">1.3</label>
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="1.4" id="inactif-middle" required>
+                                                <label class="data-label-proposition" for="inactif-low">1.4</label>
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="1.5" id="inactif-high" required>
+                                                <label class="data-label-proposition" for="inactif-high">1.5</label>
                                         </div>
-                                        <div class="data-nap-coef__level">
-                                                <input class="data-input" type="radio" name="nap" value="minimal-minus" id="minimal-minus" required>
-                                                <label class="data-label-proposition" for="minimal-minus">1.2</label>
-                                                <input class="data-input" type="radio" name="nap" value="minimal-plus" id="minimal-plus" required>
-                                                <label class="data-label-proposition" for="minimal-plus">1.3</label>
+                                        <div class="data-nap-coef__level" v-else-if="napLevel == 'moderate'">
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="1.6" id="moderate-low" required>
+                                                <label class="data-label-proposition" for="moderate-low">1.6</label>
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="1.7" id="moderate-high" required>
+                                                <label class="data-label-proposition" for="moderate-high">1.7</label>
                                         </div>
-                                        <div class="data-nap-coef__level">
-                                                <input class="data-input" type="radio" name="nap" value="moderate-minus" id="moderate-minus" required>
-                                                <label class="data-label-proposition" for="moderate-minus">1.4</label>
-                                                <input class="data-input" type="radio" name="nap" value="moderate-plus" id="moderate-plus" required>
-                                                <label class="data-label-proposition" for="moderate-plus">1.5</label>
+                                        <div class="data-nap-coef__level" v-else-if="napLevel == 'important'">
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="1.8" id="important-low" required>
+                                                <label class="data-label-proposition" for="important-low">1.8</label>
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="1.9" id="important-high" required>
+                                                <label class="data-label-proposition" for="important-high">1.9</label>
                                         </div>
-                                        <div class="data-nap-coef__level">
-                                                <input class="data-input" type="radio" name="nap" value="high-minus" id="high-minus" required>
-                                                <label class="data-label-proposition" for="high-minus">1.5</label>
-                                                <input class="data-input" type="radio" name="nap" value="high-plus" id="high-plus" required>
-                                                <label class="data-label-proposition" for="high-plus">1.6</label>
-                                        </div>
-                                        <div class="data-nap-coef__level">
-                                                <input class="data-input" type="radio" name="nap" value="intense-minus" id="intense-minus" required>
-                                                <label class="data-label-proposition" for="intense-minus">1.7</label>
-                                                <input class="data-input" type="radio" name="nap" value="intense-plus" id="intense-plus" required>
-                                                <label class="data-label-proposition" for="intense-plus">1.8</label>
+                                        <div class="data-nap-coef__level" v-else-if="napLevel == 'intense'">
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="2" id="intense-low" required>
+                                                <label class="data-label-proposition" for="intense-low">2.0</label>
+                                                <input v-model="napCoef" class="data-input" type="radio" name="nap" value="2.2" id="intense-high" required>
+                                                <label class="data-label-proposition" for="intense-high">2.1</label>
                                         </div>
                                 </div>
                         </div>                        
                 </div>
+
+                <input type="button" v-if="napCoef" @click="metabaseCalcul" value="Calculer mes résultats intermédiaires">
         </form>
+
+        <div class="result mb-&-dej">
+                <h2>Résultats intermédiaires</h2>
+                
+                <div class="mb">
+                        <p>
+                                Votre métabolisme de base : {{ metabase  }}
+                        </p>
+                </div>
+
+                <div class="dej">
+                        <p>
+                                Votre Dépense Énergétique Journalière : {{  dej }}
+                        </p>
+                </div>
+        </div>
+
+        <form method="POST" class="user-wishes">
+
+        </form>
+
+        <div>
+                genre : {{ gender }} <br>
+                poids : {{ weight }} <br>
+                taille :{{ height }} <br>
+                age : {{ age }} <br>
+                nap-level : {{ napLevel }} <br>
+                nap-coef : {{ napCoef }} <br>
+                metabase : {{ metabase}}<br>
+                dej : {{ dej }}<br>
+        </div>
 
 </section>
 </template>
+
+<script>
+
+export default {
+        data() {
+                return {
+                        gender: null,
+                        weight: null,
+                        weightManCoef: 13.707,
+                        weightWomanCoef: 9.740,
+                        height: null,
+                        heightManCoef: 492.3,
+                        heightWomanCoef: 172.9,
+                        age: null,
+                        ageManCoef: 6.673,
+                        ageWomanCoef: 4.737,
+                        additionnalManCoef: 77.607,
+                        additionnalWomanCoef: 667.051,
+                        napLevel: null,
+                        napCoef: null,
+                        metabase: null,
+                        dej: null,
+                }
+        },
+
+        methods: {
+                metabaseCalcul() {
+                        let metabaseResult = null;
+
+                        if (this.gender === "man") {
+                                metabaseResult = ((this.weight * this.weightManCoef) + (this.height * this.heightManCoef) - (this.age * this.ageManCoef) + this.additionnalManCoef);
+                        } else if (this.gender === "woman") {
+                                metabaseResult = ((this.weight * this.weightWomanCoef) + (this.height * this.heightWomanCoef) - (this.age * this.ageWomanCoef) + this.additionnalWomanCoef);
+                        }
+                        
+                        this.metabase = Math.round(metabaseResult);
+                        this.dejCalcul(this.metabase, this.napCoef);
+                },
+
+                dejCalcul(mb, activity) {
+                        const dejResult = mb * activity;
+                        this.dej = Math.round(dejResult);
+
+                        console.log(this.dej);
+                }
+        }
+}
+
+</script>
