672fadeebbd869d796faa14d70bbfb99676dd6dc
César MORO <cesar.moro.dev@gmail.com>
2023-05-30 17:27:45 +0200

    Commencement du back-end avec Node.js et la Nutrition API de API Ninjas. Revenir sur ce développement dès que possible

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,5 +1,7 @@
 /* Chercher le package Express dans les node_modules */
 import Express from "express";
+/* Chercher le package Request (vers la Nutrition API) */
+const request = require('request');
 
 /* Créer une instance d'Express */
 const app = Express();
@@ -10,6 +12,11 @@ const port = 3000;
 qui entend l'objet de la requête et l'objet de la demande.
 La réponse utilise la méthode "send()" pour renvoyer quelque chose */
 app.get('/', (req, res) => res.send("Ceci est la route par défaut (\"/\")"));
+
+app.get('https://api.api-ninjas.com/v1/nutrition', (req, res) => {
+        console.log(res);
+        res.send("On a une réponse");
+})
 
 /* Écouter l'application sur le port donné */
 app.listen(port, () => console.log(`Nickel ! L'application est démarrée sur : http://localhost:${port}`));
